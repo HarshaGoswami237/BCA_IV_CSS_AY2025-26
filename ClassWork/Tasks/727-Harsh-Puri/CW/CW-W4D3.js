12d4fd2892af1775f16ee0e508e7c1c265ba23c5
HARSH <HARSH@AYUSHI>
2026-03-13 12:07:14 +0530

    CW d3: date operations

---
+++ ClassWork/Tasks/727-Harsh-Puri/CW/CW-W4D3.js
@@ -0,0 +1,14 @@
+//Build a countdown timer a for a target date
+let today = new Date()
+console.log(today)
+// create birthdpecific date
+const birthday = new Date("2006-06-02")
+const birthdayTimestamp = birthday.getTime()
+const nextCristmas = new Date("2026-12-25")
+console.log(birthday)
+console.log(birthdayTimestamp)
+console.log(nextCristmas)
+
+//timstamp 
+let timestamp = Date.now();
+console.log(timestamp)
