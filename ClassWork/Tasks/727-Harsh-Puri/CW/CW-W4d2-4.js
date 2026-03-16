f6fd4127f85f74bf962bab66a06018c38b3c74f2
HARSH <HARSH@AYUSHI>
2026-03-16 11:41:54 +0530

    Array Removing duplicates

--- ClassWork/Tasks/727-Harsh-Puri/CW/CW-W4d2-4.js
+++ ClassWork/Tasks/727-Harsh-Puri/CW/CW-W4d2-4.js
@@ -1,5 +1,8 @@
 //Advance Array operaations 
 //1. removing operations from arrays
+
+console.log("\t ===== Element removing =====\n")
+
 //Method 1 : using filter() - non-distructive
 console.log("\tMethod 1:\n")
 const numbers = [15,52,69,8,3,19,22,4]
@@ -48,16 +51,73 @@ const fruits2 = [ "Chili" , "berry" , "apple" , "tomaato"  ]
 console.log(removingFirstOccursion(fruits2 , "Chili" ))
 
 //2. merging arrays
+console.log("\t ===== Array Merging =====\n")
+const arr1 = [34,56,86]
+const arr2 = [60,22,92] 
+
 //Method 1: using cancat()
+console.log("\tMethod 1: Concat\n")
+const merged =arr.concat(arr2)
+    console.log("Array 1 before merge : ",arr1)
+    console.log("Array 2 before merge : ",arr2)
+    console.log("Array  After merge : ",merged)
+
+
 //Method 2: spread operator
+console.log("\n\tMethod 2: spread operator \n")
+const merged2 = [...arr1,...arr2]
+
+    console.log("Array 1 before merge : ",arr1)
+    console.log("Array 2 before merge : ",arr2)
+    console.log("Array  After merge 2: ",merged2)
+
+
 //Method 3: using multiple arrays
 
+console.log("\n\tMethod 3: using multiple array \n")
+const merged3 = [...arr1,...arr2 , ...[23,44,55,78,54,32]]
+
+    console.log("Array 1 before merge : ",arr1)
+    console.log("Array 2 before merge : ",arr2)
+    console.log("Array  After merge 3 : ",merged3)
+
+
 //3. removing duplicates
+    console.log("\t ===== Removing Duplicates =====\n")
+    const nums = [1,2,8,4,1,2,7,8,9]
+
 //Method 1: using set
+console.log("\n\tMethod 1: using Set \n")
+    const uniqueSet = new Set(nums)
+    const uniqueArr  = [...uniqueSet]
+
+        console.log("Orignal Array : ",nums)
+        console.log("Array with unique Value : ",uniqueArr)
+        const newUnique = [...new Set(nums)]
+        
+        console.log("New Unique Arra : ",newUnique)
+
+
 //Method 2: using filter()
-//Method 3: using reduce()
+        console.log("\n\tMethod 2: using filter \n")
+
+ const  unq = nums.filter((number,idx ) => nums.indexOf(number) == idx  )
+
+        console.log("Orignal Array : ",nums) 
+        console.log("Array with unique Value  by filter: ",unq)
+
+ //Method 3: using reduce()
+        const rd_unique =  nums.reduce( ( acc,num )=>  {
+            if(!acc.includes(num)){
+        acc.push()
+            }
+            return acc
+    }, [] )
+        console.log("Orignal Array : ",nums) 
+        console.log("Array with unique Value  by filter: ",rd_unique)
 
 //4. sorting arrays
+
 //numeric sorting
 //Reverse order sorting
 //string sorting
